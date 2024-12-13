937f63bf46dc560884ebc07efa58b693295b7e45
DeepanshuSharma3110 <in.prince.sharma@gmail.com>
2024-12-13 10:30:05 +0530

    starting day  2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import app from './index.js';
-
+import mongoose  from 'mongoose';
 const port = process.env.PORT;
 console.log(port);
+const dbUri = process.env.DB;
+mongoose.connect()
 
 app.listen(port,()=>{
     console.log('server is running at port no ',port);
